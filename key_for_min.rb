c196ea9355ff84ecd4bac6840d1b6b0cdaac9530
Yuta Katsuyama <ykatsuyama@id.iit.edu>
2019-04-24 01:13:42 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,6 +3,10 @@
 
 def key_for_min_value(name_hash)
   name_hash.each do |product, price|
-    if price<
+    min = 0
+    if min>#{price}
 
+if (min > n[i]) {	//現在の最小値よりも小さい値が出たら
+				min = n[i];	//変数minに値を入れ替える
+			}
 end
