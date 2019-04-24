6c5dea83b1e48e540ca4de36bae8e7313083300e
Yuta Katsuyama <ykatsuyama@id.iit.edu>
2019-04-24 01:26:30 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,11 +1,8 @@
-# prereqs: iterators, hashes, conditional logic
-# Given a hash with numeric values, return the key for the smallest value
-
 def key_for_min_value(name_hash)
   lowest_key = nil
   lowest_value = nil
    name_hash.each do |k, v|
-     if lowest_value==nil || v < lowest_value
+     if lowest_value == nil || v < lowest_value
      lowest_value = v
      lowest_key = k
    　end
