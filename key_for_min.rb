f8367cac2f27fe7d0b5cab2b9c198909dea195cf
Yuta Katsuyama <ykatsuyama@id.iit.edu>
2019-04-24 01:27:32 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,12 +1,1 @@
-def key_for_min_value(name_hash)
-  lowest_key = nil
-  lowest_value = nil
-   name_hash.each do |k, v|
-     if lowest_value == nil || v < lowest_value
-     lowest_value = v
-     lowest_key = k
-   　end
-   end
-   lowest_key
-end
 
