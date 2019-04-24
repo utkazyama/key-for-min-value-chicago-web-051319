a3cd760b903eee0d4fb182a50adc2610f89f0647
Yuta Katsuyama <ykatsuyama@id.iit.edu>
2019-04-24 01:19:59 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,11 +2,11 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  def min=0
   name_hash.each do |product, price|
+   min=price[0]
     if min>price
     min=price
   　end
   end
-  print min
+  print #{min}
 end
